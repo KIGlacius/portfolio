72626ae2329c6cb5cc12e8d0db8652e36b318dfa
KIGlacius <kwhite11419@gmail.com>
2021-10-20 16:18:11 -0400

    init

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+const hbs = require('hbs');
+hbs.registerPartials(partialsPath);
+const app = express();
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+app.use(express.static(publicDirectoryPath));
+app.get('', (req, res) => {
+  res.render('index');
+});
+
+app.get('/triviagame', (req, res) => {
+  res.render('triviagame');
+});
+
+app.get('/timeup', (req, res) => {
+  res.render('timeup');
+});
+
+app.get('*', (req, res) => {
+  res.render('404');
+});
+
+app.listen(3000);
