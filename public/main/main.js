f409313e24a1f25ba56c0af649af0ed8f7889c21
KIGlacius <kwhite11419@gmail.com>
2021-11-17 12:46:13 -0500

    revamped landing page

--- public/main/main.js
+++ public/main/main.js
@@ -1,31 +1,107 @@
 const init = function () {
-  let n = 1;
+  const namePopinKayl = document.querySelector(".name-popin__kayl");
+  const namePopinKaradjian = document.querySelector(".name-popin__karadjian");
+  const arrowDownSection1 = document.querySelector(".angle-down-svg");
+  const arrowUpMain = document.querySelector(".angles-up-svg");
+  const section1 = document.querySelector(".section-1");
+  const slides = document.querySelectorAll(".project-slider");
+  const allSections = document.querySelectorAll(".app__container");
+  const arrowLeft = document.querySelector(".left-arrow");
+  const arrowRight = document.querySelector(".right-arrow");
+  const body = document.querySelector("body");
+  let curSlide = 0;
 
-  const changePicStart = () => {
-    const pic = document.querySelector(`.pic-${n - 1}`);
-    const newPic = document.querySelector(`.pic-${n}`);
-    pic.classList.add("hidden");
-    newPic.classList.remove("hidden");
-    newPic.style.transition = "all 2s";
-  };
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      namePopinKayl.style.left = "0";
+      namePopinKayl.style.transition = "1s";
+      namePopinKayl.style.transitionTimingFunction = "ease-out";
+      namePopinKaradjian.style.right = "0";
+      namePopinKaradjian.style.transition = "1s";
+      namePopinKaradjian.style.transitionTimingFunction = "ease-out";
+    }, 750);
+  });
 
-  const changePicEnd = () => {
-    const pic = document.querySelector(`.pic-${n - 1}`);
-    pic.classList.add("hidden");
-    n = 1;
-    const newPic = document.querySelector(`.pic-${n}`);
-    newPic.classList.remove("hidden");
-    newPic.style.transition = "all 2s";
-  };
+  arrowDownSection1.addEventListener("click", () => {
+    section1.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
+  });
+
+  arrowUpMain.addEventListener("click", () => {
+    body.scrollIntoView({ behavior: "smooth" });
+  });
 
   setInterval(() => {
-    n++;
-    if (n > 4) {
-      changePicEnd();
-    } else {
-      changePicStart();
+    arrowDownSection1.style.marginBottom = "35px";
+    arrowDownSection1.style.transition = ".5s";
+    arrowDownSection1.style.transitionTimingFunction = "ease-out";
+  }, 1000);
+
+  setInterval(() => {
+    arrowDownSection1.style.marginBottom = "25px";
+  }, 2000);
+
+  setInterval(() => {
+    arrowUpMain.style.marginTop = "25px";
+  }, 1000);
+
+  setInterval(() => {
+    arrowUpMain.style.marginTop = "35px";
+    arrowUpMain.style.transition = ".5s";
+    arrowUpMain.style.transitionTimingFunction = "ease-out";
+  }, 2000);
+
+  const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    if (entry.isIntersecting === true) {
+      entry.target.classList.remove("hidden-section");
+      entry.target.style.transition = ".5s";
     }
-  }, 3000);
+
+    if (entry.isIntersecting === false) {
+      entry.target.classList.add("hidden-section");
+      entry.target.style.transition = ".5s";
+    }
+  };
+
+  const obsOptions = {
+    root: null,
+    threshold: 0.5,
+  };
+
+  const observer = new IntersectionObserver(revealSection, obsOptions);
+  allSections.forEach(function (section) {
+    observer.observe(section);
+    section.classList.add("hidden-section");
+  });
+
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX(${100 * i}%)`;
+  });
+
+  arrowLeft.addEventListener("click", () => {
+    if (curSlide === 0) {
+      curSlide = slides.length - 1;
+    } else {
+      curSlide--;
+    }
+    slides.forEach((s, i) => {
+      s.style.transform = `translateX(${100 * (i - curSlide)}%)`;
+    });
+  });
+
+  arrowRight.addEventListener("click", () => {
+    if (curSlide === slides.length - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+    slides.forEach((s, i) => {
+      s.style.transform = `translateX(${100 * (i - curSlide)}%)`;
+    });
+  });
 };
 
 init();
